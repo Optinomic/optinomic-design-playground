5685843a7b7ee733c638d354292075d6c32ffa4a
Beat Ottiger <ottigerb@gmail.com>
2014-08-26 19:55:11 +0200

    Updated factory

--- app/angular_scripts/factory.js
+++ app/angular_scripts/factory.js
@@ -78,16 +78,25 @@ app.factory('app_factory', function() {
         }],
 
         modules_array: [{
+            "id": "1",
             "name": "Beck-Depressions-Inventar (BDI-II)",
             "description": "Schweregrad depressiver Symptomatik im klinischen Bereich, 21 Aussagen. Ein etwas längerer Text.",
             "full_description": "Das Beck-Depressions-Inventar (BDI – Beck, Ward, Mendelson, Mock & Erbaugh, 1961) ist ein psychologisches Testverfahren, das die Schwere depressiver Symptomatik im klinischen Bereich erfasst. Es wurde nach Aaron T. Beck benannt. In dem Fragebogen mit 21 Fragen bestimmt der Teilnehmer, welche der vier Aussagen für ihn/sie in dieser Woche am zutreffendsten ist. Zur Auswertung werden die Summenwerte der einzelnen angekreuzten Aussagen addiert. Im Laufe der Zeit wurde das zuerst als standardisiertes Interview gedachte Verfahren zu zwei bearbeiteten Formen weiterentwickelt, dem BDI – 1A (1978) und dem BDI – II (1996). Ergänzend zu diesen beiden Formen gibt es seit Herbst 2013 eine Kurzform der BDI-II in deutscher Sprache, die sich BDI-FS (FS steht für FastScreen) nennt, und die Schwere einer Depression auf Basis der nicht-somatischen Symptome erfasst.",
             "responsibility": "Therapeut",
             "schedule": "Alle 14 Tage"
         }, {
+            "id": "2",
             "name": "Stress- und Coping-Inventar (SCI)",
             "description": "Messung von Stressbelastung, Stresssymptomen und Stressbewältigungs-Strategien (Coping). Stressbelastung, Stresssymptomen und Stressbewältigungs-Strategien",
             "responsibility": "Patient",
             "schedule": "Bei Eintritt"
+        }, {
+            "id": "3",
+            "name": "DIAD",
+            "description": "Differentielle klinische Diagnostik adoleszentärer Dissozialisation. Kurzform.",
+            "full_description": "Weitere Detailbeschreibung: Differentielle klinische Diagnostik adoleszentärer Dissozialisation. Kurzform.",
+            "responsibility": "Gruppe: Therapeuten",
+            "schedule": "Immer wieder"
         }]
 
 
